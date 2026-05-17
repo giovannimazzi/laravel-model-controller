37c9602e584e4b4c28fa0c278383f9aa384b82bc
giovannimazzi <gioelor@libero.it>
2026-05-17 03:03:46 +0200

    add movie-card component

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
     <div class="container py-3">
-        <div class="card mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Success card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            </div>
-            <div class="card-footer bg-primary bg-opacity-25 text-primary">Footer</div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+              @foreach ($movies as $movie)
+                <div class="col">
+                    <x-movie-card/>   
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
